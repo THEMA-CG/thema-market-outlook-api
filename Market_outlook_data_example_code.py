6a34d27119e417659a5243673c17f8b85795618a
Mathias Nipen <mathias.nipen@thema.no>
2025-10-01 12:21:15 +0200

    Capitalized transition and used September 2025 edition

--- Market_outlook_data_example_code.py
+++ Market_outlook_data_example_code.py
@@ -44,11 +44,11 @@ hourly_data.to_excel(f"{output_folder}Hourly_data.xlsx", index=False)
 # parameters that are not specified, or given as None, will be filled with all valid inputs by program. 
 # Note that this might lead to high memory usage and long execution time
 json = {
-    "scenario": {"Base", "Turbulent transition", "Green Growth"},
+    "scenario": {"Base", "Turbulent Transition", "Green Growth"},
     "group": {"Real prices", "Generation"},
     "indicator": {"Gas price", "Coal price", "Nuclear"},
     "region": "Nordics",
-    "edition": "September 2022",
+    "edition": "September 2025",
     "country": {"Norway", "Sweden"},
     "zone": {"NO1", "NO2", "SE1", "SE2", "SE3", "SE4"}
         }
